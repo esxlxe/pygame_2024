a0e929d67c45894749ce74219afd50ed72bb804c
esxlxe <145538923+esxlxe@users.noreply.github.com>
2024-01-29 18:54:44 +0500

    Update main.py

--- lesson_8/main.py
+++ lesson_8/main.py
@@ -1,10 +1,10 @@
 import pygame
 
-SCREEN_SIZE = (400, 300)
-FPS = 60
-
 # инициализация Pygame / как завести ключ зажигания
 pygame.init()
+
+SCREEN_SIZE = (400, 300)
+FPS = 60
 
 # отобразить экран
 screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -12,17 +12,15 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 # объект поможет отслеживать время
 clock = pygame.time.Clock()
 
-surface_sky = pygame.image.load("graphics/sky.png").convert()
-surface_ground = pygame.image.load("graphics/ground.png").convert()
+test_surface = pygame.Surface((100, 200))
+test_surface.fill((255,0,255)) # RGB
 
 while True:
     # эвэнт цикл
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
-    screen.blit(surface_sky, (0, 0))
-    screen.blit(surface_ground, (0, 220))
+    screen.blit(test_surface, (0,0))
 
     pygame.display.update()
 
